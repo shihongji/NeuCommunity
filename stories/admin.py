32187a330664ae7590239103fa724adba66a6bc4
hongji <shihongji21@gmail.com>
2023-03-26 13:04:07 -0700

    Change Upvote style

--- stories/admin.py
+++ stories/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Story, Category, Tag, Comment
+from .models import Story, Category, Tag, Comment, Vote
 # Register your models here.
 
 admin.site.register(Story)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Comment)
+admin.site.register(Vote)
